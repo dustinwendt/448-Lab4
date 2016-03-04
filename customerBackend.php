21b119ea99d6e4fe9b58882cb5436a7a66cbff08
Dustin Wendt <dustinwendt@ku.edu>
2016-03-03 23:01:32 -0600

    Add validation for inputs Add backEnd for valid input

---
+++ customerBackend.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+	<?php
+
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+
+	$gibsonAmount = _POST['gibson'];
+	$fenderAmount = _POST['fender'];
+	$epiphoneAmount = _POST['epiphone'];
+
+	$username = _POST['username'];
+	$password = _POST['password'];
+
+	$shippingName = array ("7-Day", "Overnight", "3-Day");
+	$shippingCost = array (0,50,5);
+	$shipping = _POST['shipping'];
+
+	$total = $gibsonAmount * 1299.00 + $fenderAmount * 1799.99 + $epiphoneAmount * 499.00 + $shippingCost[$shipping];
+
+	echo "Username: " . $username;
+	echo "Password: " . $password . "<br>";
+
+	echo "<table";
+	echo "<tr><td></td> <td>Quantity</td> <td>Cost per Item</td> <td>Subtotal</td> </tr>";
+	echo "<tr> <td>Gibson</td><td>" . $gibsonAmount . "</td><td>$1,299.00</td> <td>$" . $gibsonAmount * 1299.00 . "</td> </tr>";
+	echo "<tr> <td>Fender</td><td>" . $fenderAmount . "</td><td>$1,799.99</td> <td>$" . $fenderAmount * 1799.99 . "</td> </tr>";
+	echo "<tr> <td>Epiphone</td><td>" . $epiphoneAmount . "</td><td>$499.00</td><td>$" . $epiphoneAmount * 499.00 . "</td></tr>";
+	echo "<tr> <td>Shipping</td> <td colspan = "2">" . $shippingName[$shipping] . "</td><td>$" . $shippingCost[$shipping] .  "</td></tr>";
+	echo "<tr><td colspan="3">Total Cost</td><td>$" . . "</td></tr>"
+
+	echo "</table>";
+	?>
+</body>
+
+</html>
---
+++ formChecker.js
@@ -0,0 +1,50 @@
+function reset()
+{
+	document.getElementById('gibson').value = 0;
+	document.getElementById('fender').value = 0;
+	document.getElementById('epiphone').value = 0;
+	return false;
+}
+
+function shippingValidate()
+{
+	var shippingChoice = document.store.shipping;
+	for(i=0;i<shippingChoice.length;i++)
+	{
+		if(shippingChoice[i].checked == true)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+function validate()
+{
+	var num_gib document.getElementById('gibson').value;
+	var num_fen document.getElementById('fender').value;
+	var num_epi document.getElementById('epiphone').value;
+
+	if(num_gib < 0 || num_fen < 0 || num_epi <0)
+	{
+		alert("Quantities cannot be nonzero");
+		reset();
+		return false;
+	}
+	else if(!shippingValidate())
+	{
+		alert("Please select a shipping option");
+		return false;
+	}
+	else if(document.getElementById('username').value.indexOf('@') == -1 || document.getElementById('username').value.indexOf('.') == -1)
+	{
+		alert("Please enter a username in the form of a valid e-mail");
+		return false;
+	}
+	else if(document.getElementById('password').value == '')
+	{
+		alert("Please enter a password");
+		return false;
+	}
+	return true;
+}
